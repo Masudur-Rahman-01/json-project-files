39bd3429ce1571822680420ef76901853f25961f
masudurrahman <masudurrtr01@gmail.com>
2021-12-14 17:54:53 +0600

    Upload the file

---
+++ files/main.js
@@ -0,0 +1,17 @@
+
+//-01 Students data
+
+const data1 = '[{"Id":1,"Roll":1,"Name":"Ahmed Yousuf","Age":12,"Location":"Mirpur","Marks":[{"bn":60,"en":70,"math":85,"sc":55,"ss":74,"rel":80}]},{"Id":2,"Roll":2,"Name":"Azom Ali","Age":11,"Location":"Mirpur","Marks":[{"bn":70,"en":70,"math":85,"sc":60,"ss":74,"rel":80}]},{"Id":3,"Roll":3,"Name":"Ahammad Ali","Age":12,"Location":"Mirpur","Marks":[{"bn":80,"en":70,"math":85,"sc":55,"ss":74,"rel":50}]},{"Id":4,"Roll":4,"Name":"Ali Asgor","Age":12,"Location":"Banani","Marks":[{"bn":80,"en":70,"math":40,"sc":55,"ss":74,"rel":80}]},{"Id":5,"Roll":5,"Name":"Jamshed Ali","Age":12,"Location":"Banani","Marks":[{"bn":80,"en":70,"math":40,"sc":55,"ss":74,"rel":80}]},{"Id":6,"Roll":6,"Name":"Lalon Shaikh","Age":12,"Location":"Banani","Marks":[{"bn":80,"en":60,"math":40,"sc":55,"ss":74,"rel":80}]},{"Id":7,"Roll":7,"Name":"Rasel Shaikh","Age":12,"Location":"Uttara","Marks":[{"bn":90,"en":70,"math":40,"sc":55,"ss":60,"rel":80}]},{"Id":8,"Roll":8,"Name":"Rashed Alom","Age":12,"Location":"Uttara","Marks":[{"bn":80,"en":70,"math":80,"sc":55,"ss":74,"rel":80}]},{"Id":9,"Roll":9,"Name":"Alom Ali","Age":12,"Location":"Uttara","Marks":[{"bn":80,"en":70,"math":40,"sc":55,"ss":74,"rel":80}]},{"Id":10,"Roll":10,"Name":"Shah Alom","Age":12,"Location":"Uttara","Marks":[{"bn":80,"en":60,"math":70,"sc":55,"ss":74,"rel":80}]}]';
+
+//-02 PhoneBooks data
+
+const data2 = '[{"Id":1,"Name":"Jamshed Ahammed","Relation":"Brother","Location":"Mirpur","Phone":"01719295653"},{"Id":2,"Name":"Ali Ahammed","Relation":"Brother","Location":"Mirpur","Phone":"01712295653"},{"Id":3,"Name":"Karim Ahammed","Relation":"Brother","Location":"Mirpur","Phone":"01711295653"},{"Id":4,"Name":"Taimur Karim","Relation":"Friend","Location":"Uttara","Phone":"01839895653"},{"Id":5,"Name":"Sagor Ahmed","Relation":"Friend","Location":"Uttara","Phone":"01719295653"},{"Id":6,"Name":"Milon Shah","Relation":"Cousin","Location":"Mirpur","Phone":"01719295653"},{"Id":7,"Name":"Rajon Shah","Relation":"Cousin","Location":"Mirpur","Phone":"01712956530"},{"Id":8,"Name":"Shah Alom","Relation":"Friend","Location":"Mirpur","Phone":"01711295653"},{"Id":9,"Name":"Jaman Ali","Relation":"Friend","Location":"Uttara","Phone":"01839895653"},{"Id":10,"Name":"Mamun Alom","Relation":"Friend","Location":"Banani","Phone":"01717295653"}]';
+
+//-03 Developers Data
+
+const data3 = '[{"Id":1,"Name":"Mamun Alom","Location":"Banani","Skill":"Java","Income":20000},{"Id":2,"Name":"Yousuf Rana","Location":"Uttara","Skill":"Python","Income":18000},{"Id":3,"Name":"Tamim Hasan","Location":"Mirpur","Skill":"Django","Income":15000},{"Id":4,"Name":"Rahim Ullah","Location":"Banani","Skill":"Python","Income":20000},{"Id":5,"Name":"Shahin Ali","Location":"Banani","Skill":"Mern Stack","Income":20000},{"Id":6,"Name":"Mamun Shikdar","Location":"Banani","Skill":"Java","Income":20000},{"Id":7,"Name":"Yousuf Ali","Location":"Uttara","Skill":"Python","Income":18000},{"Id":8,"Name":"Riaz Haidar","Location":"Mirpur","Skill":"Django","Income":15000},{"Id":9,"Name":"Taleb Ullah","Location":"Banani","Skill":"Python","Income":25000},{"Id":10,"Name":"Romjan Ali","Location":"Banani","Skill":"Mern Stack","Income":20000}]';
+
+const all = [data1, data2, data3];
+
+console.log(all);
+
